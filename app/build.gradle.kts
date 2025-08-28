b8b41c0844087f604b9df988e4bb6992e40f1d6e
darblee <darblee123@gmail.com>
2025-08-27 23:55:23 -0700

    Update to the latest components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "com.darblee.livingword"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.darblee.livingword"
         minSdk = 34
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
